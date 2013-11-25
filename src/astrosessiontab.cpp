96780dde565c3d3df0416fcb1eeac83b67d3e296
Marco Gulino <marco.gulino@gmail.com>
2013-11-25 17:50:09 +0100

    AstroSessionTab widget

---
+++ src/astrosessiontab.cpp
@@ -0,0 +1,42 @@
+/*
+ * <one line to give the program's name and a brief idea of what it does.>
+ * Copyright (C) 2013  <copyright holder> <email>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "Models"
+#include "astrosessiontab.h"
+#include "private/astrosessiontab_p.h"
+#include "utils/d_ptr_implementation.h"
+#include "Wt-Commons/wt_helpers.h"
+#include "session.h"
+
+using namespace Wt;
+using namespace WtCommons;
+using namespace std;
+
+AstroSessionTab::Private::Private(const Dbo::ptr<AstroSession>& astroSession, Session &session, AstroSessionTab* q) : astroSession(astroSession), session(session), q(q)
+{
+}
+
+AstroSessionTab::~AstroSessionTab()
+{
+}
+
+AstroSessionTab::AstroSessionTab(const Dbo::ptr<AstroSession>& astroSession, Session& session, WContainerWidget* parent)
+    : WContainerWidget(parent), d(astroSession, session, this)
+{
+}
