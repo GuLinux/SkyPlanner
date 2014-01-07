f56a0d0ca0fd2e4bd336e16c1198829ed7f51c60
Marco Gulino <marco.gulino@gmail.com>
2014-01-07 23:12:26 +0100

    Enlarged field for DSS++

--- src/widgets/dssimage.cpp
+++ src/widgets/dssimage.cpp
@@ -105,15 +105,15 @@ string DSSImage::Private::imageLink() const
 {
   Angle::Sexagesimal objectSize = size.sexagesimal();
   double objectRect = static_cast<double>(objectSize.degrees * 60) + static_cast<double>(objectSize.minutes) + (objectSize.seconds/60);
-  double multiplyFactor = 2.5;
+  double multiplyFactor = 3.0;
   if(objectRect < 20)
-    multiplyFactor = 3.5;
+    multiplyFactor = 5.0;
   if(objectRect < 10)
-    multiplyFactor = 4.5;
+    multiplyFactor = 6.5;
   if(objectRect < 5 )
-    multiplyFactor = 6.;
-  if(objectRect < 1 )
     multiplyFactor = 10.;
+  if(objectRect < 1 )
+    multiplyFactor = 20.;
   
   objectRect = min(75.0, objectRect * multiplyFactor);
   objectRect = objectRect <= 0 ? 75.0:objectRect; // objects without angular size (-1), showing max possible field...
