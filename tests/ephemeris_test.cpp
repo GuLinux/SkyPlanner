98c0ae4e1a9ddd89097e123b4b32eac738e170a2
Marco Gulino <marco.gulino@gmail.com>
2014-03-26 11:42:29 +0100

    Fixed and improved tests

--- tests/ephemeris_test.cpp
+++ tests/ephemeris_test.cpp
@@ -7,24 +7,25 @@ using namespace std;
 
 
 
-BOOST_AUTO_TEST_CASE(TestAndromedaEquatorialToAltAz)
-{
-  double andromedaArAsRad = 0.186314;
-  double andromedaDecAsRad = 0.720239;
-  double latitude = 44.6411166497765;
-  double longitude = 9.2164134979248;
-  Ephemeris ephemeris({Angle::degrees(latitude), Angle::degrees(longitude)});
-  Coordinates::AltAzimuth altAz = ephemeris.arDec2altAz({Angle::radians(andromedaArAsRad), Angle::radians(andromedaDecAsRad)}, boost::posix_time::time_from_string("2013-11-28 20:04:00"));
-  cerr << "alt: " << altAz.altitude.degrees() << ", az: " << altAz.azimuth.degrees() << endl;
-}
-
 BOOST_AUTO_TEST_CASE(TestSunRiseSet)
 {
   double latitude = 44.6427;
   double longitude = 9.2150;
-  Ephemeris ephemeris({Angle::degrees(latitude), Angle::degrees(longitude)});
-  auto sun = ephemeris.sun(boost::posix_time::time_from_string("2014-02-26 22:22:11"), false);
-  auto sunAstro = ephemeris.astronomicalTwilight(boost::posix_time::time_from_string("2014-02-26 22:22:11"), false);
+  boost::posix_time::ptime when({2014, 03, 29});
+  Ephemeris ephemeris({Angle::degrees(latitude), Angle::degrees(longitude)}, {});
+  Ephemeris::RiseTransitSet sun = ephemeris.sun(when);
+  Ephemeris::RiseTransitSet astroTwilight = ephemeris.astronomicalTwilight(when);
+  Ephemeris::RiseTransitSet moon = ephemeris.moon(when);
+  auto darkness = ephemeris.darknessHours(when );
+  Ephemeris::LunarPhase phase = ephemeris.moonPhase(when);
+  double firstFraction = phase.illuminated_fraction;
+  for(int i=0; i<1000; i++) {
+    phase = ephemeris.moonPhase(when);
+    BOOST_REQUIRE_CLOSE(firstFraction, phase.illuminated_fraction, 0.0001);
+  }
+
   cerr << "Sun: rise=" << sun.rise << ", transit=" << sun.transit << ", set=" << sun.set << endl;
-  cerr << "Astronomical twilight: rise=" << sunAstro.rise << ", transit=" << sunAstro.transit << ", set=" << sunAstro.set << endl;
+  cerr << "moon: rise=" << moon.rise << ", transit=" << moon.transit << ", set=" << moon.set << endl;
+  cerr << "Astronomical twilight: rise=" << astroTwilight.rise << ", transit=" << astroTwilight.transit << ", set=" << astroTwilight.set << endl;
+  cerr << "Darkness: begin=" << darkness.begin << ", end=" << darkness.end << ", duration=" << darkness.duration << endl;
 }
