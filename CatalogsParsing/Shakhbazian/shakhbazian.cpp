b692b1e78494e8eac4e046361c94d321ee8c0e2d
Marco Gulino <marco.gulino@gmail.com>
2014-04-04 15:11:34 +0200

    compilation fix

--- CatalogsParsing/Shakhbazian/shakhbazian.cpp
+++ CatalogsParsing/Shakhbazian/shakhbazian.cpp
@@ -215,7 +215,7 @@ int main(int argc, char ** argv){
     return 0;
   }
 
-  //Dbo::Session session;
+  Dbo::Session session;
 //  Session session;
   string connectionString = app.arguments()[2].toStdString();
   shared_ptr<Dbo::SqlConnection> connection;
