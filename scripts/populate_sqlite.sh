02e632fa7916050af30431026b25c6c238910069
Marco Gulino <m.gulino@sitradeitalia.com>
2014-07-21 17:10:12 +0200

    Bash script to populate sqlite database from postgresql dump

---
+++ scripts/populate_sqlite.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+sqlite_file="$1"
+
+
+if ! [ -r "$1" ]; then
+  echo "SQLITE3 File $1 not existing; exiting"
+  exit 1
+fi
+
+temp_file="/tmp/$( basename "$1")"
+cp "$1" "$temp_file"
+sqlite3 "$temp_file" <<< "ALTER TABLE objects ADD COLUMN coordinates_geom TEXT;"
+
+temp_sql="/tmp/$( basename "$0" ).sql"
+bzcat "$( dirname "$( dirname "$( readlink -f "$0")" )")/data/objects_dump.sql.bz2" | grep -v 'SET ' | grep -v 'pg_' > "$temp_sql"
+pv -pet "$temp_sql" | sqlite3 "$temp_file"
+cp "$temp_file" "$1"
+rm "$temp_file"
+rm "$temp_sql"
+
