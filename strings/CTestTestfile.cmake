72e4fdd711bb046b2f53167107e04ff5d9998e34
Marco Gulino <marco.gulino@gmail.com>
2014-10-05 18:49:20 +0200

    Removed wrong files

--- strings/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hdd/home/marco/Sviluppo/Witty/SkyPlanner/strings
-# Build directory: /home/hdd/home/marco/Sviluppo/Witty/SkyPlanner/build/strings
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
