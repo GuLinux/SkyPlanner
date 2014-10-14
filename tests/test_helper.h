0f26576c044e38d9bd0660e8681de6b1613ed2eb
Marco Gulino <marco@gulinux.net>
2014-10-14 22:22:19 +0200

    Wt-Commons as static library

--- tests/test_helper.h
+++ tests/test_helper.h
@@ -1,7 +1,7 @@
 #ifndef BOOST_TESTS_HELPER
 #define BOOST_TESTS_HELPER
 #include <boost/test/unit_test.hpp>
-#ifdef IN_KDEVELOP_PARSER
+#ifdef IN_IDE_PARSER
 #define BOOST_CHECK_WITH_ARGS_IMPL(...) (void) __VA_ARGS__
-#endif
+#endif // IN_IDE_PARSER
 #endif
