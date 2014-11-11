5eb0312b2173a62ec058cec3532a475d118b6c5b
Marco Gulino <marco@gulinux.net>
2014-11-11 19:36:48 +0100

    Better dump script; updated dump file

--- data/createDump.sh
+++ data/createDump.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-pg_dump -h localhost -p 5432 -U skyplanner skyplanner -t objects -t denominations -t catalogues -a --column-inserts --inserts | bzip2 > objects_dump.sql.bz2
+HOST="${1-localhost}"
+PORT="${2-5432}"
+pg_dump -h $HOST -p $PORT -U SkyPlanner SkyPlanner -t objects -t denominations -t catalogues -a --column-inserts --inserts | bzip2 > objects_dump.sql.bz2
