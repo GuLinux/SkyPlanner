db7af725b44c18ddc702c3ec08561052ce591981
Marco Gulino <marco@gulinux.net>
2015-05-06 19:20:02 +0200

    more query fixes

--- scripts/geom_query.sql
+++ scripts/geom_query.sql
@@ -5,7 +5,7 @@ update objects set coordinates_geom = ST_GeomFromText('POINT(' || DEGREES(ra) ||
 select *, ST_AsText(ST_GeomFromText('POINT(' || DEGREES(ra) || ' ' || DEGREES(dec) || ')')) as geom2 from objects where object_id = 'NGC 224';
 
 select objects.*, string_agg(denominations.name, ', ') as names , 
-ST_Distance(coordinates_geom, (select coordinates_geom FROM objects where id = object_id = 'NGC 224') ) as dist
+ST_Distance(coordinates_geom, (select coordinates_geom FROM objects where object_id = 'NGC 224') ) as dist
  from objects INNER JOIN denominations on objects.id = denominations.objects_id 
  group by objects.id
  order by dist asc 
