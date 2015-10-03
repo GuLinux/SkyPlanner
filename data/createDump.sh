549a247d6bcb01117267c6e5848b8565d9b9fccb
Marco Gulino <marco@gulinux.net>
2015-10-03 20:02:50 +0200

    fix and expand dump script

--- data/createDump.sh
+++ data/createDump.sh
@@ -2,5 +2,6 @@
 HOST="${1-postgres}"
 PORT="${2-5432}"
 DB_USER="${DB_USER-postgres}"
-DB_NAME=${DB_NAME-skyplanner}"
-pg_dump -h $HOST -p $PORT -U ${DB_USER} ${DB_NAME} -t objects -t denominations -t catalogues -a --column-inserts --inserts | xz -9e > objects_dump.sql.xz
+DB_NAME="${DB_NAME-skyplanner}"
+#pg_dump -h $HOST -p $PORT -U ${DB_USER} ${DB_NAME} -t objects -t denominations -t catalogues -a --column-inserts --inserts | xz -9e > objects_dump.sql.xz
+pg_dump -h $HOST -p $PORT -U ${DB_USER} ${DB_NAME} -t objects -t denominations -t catalogues | xz -9e > objects_dump.sql.xz
