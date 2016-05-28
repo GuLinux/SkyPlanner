3e8ad224628e2c4e7d45143742f512c0e9d49cc4
Marco Gulino <marco@gulinux.net>
2016-05-28 22:22:28 +0200

    add optional smarthost relay

---
+++ docker/compose/smtp/entrypoint.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+set -e
+
+if [ "$MAILNAME" ]; then
+	echo "MAIN_HARDCODE_PRIMARY_HOSTNAME = $MAILNAME" > /etc/exim4/exim4.conf.localmacros
+	echo $MAILNAME > /etc/mailname
+fi
+
+if [ "$KEY_PATH" -a "$CERTIFICATE_PATH" ]; then
+	if [ "$MAILNAME" ]; then
+	  echo "MAIN_TLS_ENABLE = yes" >>  /etc/exim4/exim4.conf.localmacros
+	else
+		echo "MAIN_TLS_ENABLE = yes" >  /etc/exim4/exim4.conf.localmacros
+	fi
+	cp $KEY_PATH /etc/exim4/exim.key
+	cp $CERTIFICATE_PATH /etc/exim4/exim.crt
+	chgrp Debian-exim /etc/exim4/exim.key
+	chgrp Debian-exim /etc/exim4/exim.crt
+	chmod 640 /etc/exim4/exim.key
+	chmod 640 /etc/exim4/exim.crt
+fi
+
+opts=(
+	dc_local_interfaces "[0.0.0.0]:${PORT:-25} ; [::0]:${PORT:-25}"
+	dc_other_hostnames ''
+	dc_relay_nets "$(ip addr show dev eth0 | awk '$1 == "inet" { print $2 }')${RELAY_NETWORKS}"
+)
+
+if [ "$GMAIL_USER" -a "$GMAIL_PASSWORD" ]; then
+	opts+=(
+		dc_eximconfig_configtype 'smarthost'
+		dc_smarthost 'smtp.gmail.com::587'
+	)
+	echo "*.google.com:$GMAIL_USER:$GMAIL_PASSWORD" > /etc/exim4/passwd.client
+elif [ "$SES_USER" -a "$SES_PASSWORD" ]; then
+	opts+=(
+		dc_eximconfig_configtype 'smarthost'
+		dc_smarthost "email-smtp.${SES_REGION:=us-east-1}.amazonaws.com::587"
+	)
+	echo "*.amazonaws.com:$SES_USER:$SES_PASSWORD" > /etc/exim4/passwd.client
+elif [ "$SMARTHOST_USER" -a "$SMARTHOST_PASSWORD" ] && [ -n "$SMARTHOST_ADDRESS"] ; then
+	opts+=(
+		dc_eximconfig_configtype 'smarthost'
+		dc_smarthost "${SMARTHOST_ADDRESS}::${SMARTHOST_PORT-25}"
+	)
+	echo "${SMARTHOST_ADDRESS}:$SMARTHOST_USER:$SMARTHOST_PASSWORD" > /etc/exim4/passwd.client
+elif [ "$RELAY_DOMAINS" ]; then
+	opts+=(
+		dc_relay_domains "${RELAY_DOMAINS}"
+		dc_eximconfig_configtype 'internet'
+	)
+else
+	opts+=(
+		dc_eximconfig_configtype 'internet'
+	)
+fi
+
+/bin/set-exim4-update-conf "${opts[@]}"
