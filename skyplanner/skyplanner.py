fc5933f8ed0a1a4d54b410e6309b501547083725
Marco Gulino <marco@gulinux.net>
2017-01-11 18:50:40 +0000

    v2: flask app skeleton

---
+++ skyplanner/skyplanner.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+     render_template, flash
+
+app = Flask(__name__)
+
+app.config.from_envvar('SKYPLANNER_SETTINGS', silent=True)
+
+@app.route('/')
+def index():
+    return 'Hello, this is SkyPlanner skeleton'
+
+
+
