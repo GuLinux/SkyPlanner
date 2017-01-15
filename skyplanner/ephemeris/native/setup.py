ba78bc19ea7b2959664d2aa8a62f2c612ca1c167
Marco Gulino <marco@gulinux.net>
2017-01-15 22:32:07 +0000

    docker compose

--- skyplanner/ephemeris/native/setup.py
+++ skyplanner/ephemeris/native/setup.py
@@ -1,8 +1,11 @@
 from setuptools import setup, Extension
-
+import os
+boost_python3 = os.environ.get('BOOST_PYTHON3_LIB')
+if not boost_python3:
+	boost_python3 = 'boost_python3'
 module1 = Extension('ephemeris',
                     sources = ['ephemeris_main.cpp', 'ephemeris.cpp'],
-                    libraries = ['boost_python3', 'nova'],
+                    libraries = [boost_python3, 'nova'],
             extra_compile_args=['-std=c++14'])
 
 setup (name = 'Ephemeris',
