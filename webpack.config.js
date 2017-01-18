8b8f75b024d453b1478d38326f157c380436030b
Marco Gulino <marco@gulinux.net>
2017-01-18 19:27:02 +0000

    Working react-toolbox css

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,55 @@
-var path = require('path');
-var webpack = require('webpack');
- 
+const path = require('path');
+const webpack = require('webpack');
+const autoprefixer = require('autoprefixer');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+
 module.exports = {
-  output: { path: __dirname, filename: 'bundle.js' },
+  context: __dirname,
+  devtool: 'inline-source-map',
+  output: {
+    path: __dirname,
+    filename: 'bundle.js',
+    publicPath: '/static/scripts/js/'
+  },
+  resolve: {
+    extensions: ['', '.scss', '.css', '.js', '.json'],
+    modulesDirectories: [
+      'node_modules',
+      path.resolve(__dirname, './node_modules')
+    ]
+  },
   module: {
     loaders: [
       {
-        test: /.jsx?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        query: {
-          presets: ['es2015', 'react']
-        }
-      },
-      {
-          test: /\.scss$/,
-          loaders: ["style-loader", "css-loader", "sass-loader"]
+        test: /(\.js|\.jsx)$/,
+        exclude: /(node_modules)/,
+        loader: 'babel',
+        query: { presets: ['es2015', 'stage-0', 'react'] }
+      }, {
+        test: /(\.scss|\.css)$/,
+        loader: ExtractTextPlugin.extract('style', 'css?sourceMap&modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss!sass')
       }
     ]
   },
+  postcss: [autoprefixer],
+  sassLoader: {
+    data: '@import "theme/_config.scss";',
+    includePaths: [path.resolve(__dirname, './skyplanner/static/')]
+  },
+  plugins: [
+    new ExtractTextPlugin('bundle.css', { allChunks: true }),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      filename: 'vendor.bundle.js',
+      minChunks: Infinity
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('development')
+    })
+  ]
 };
+
