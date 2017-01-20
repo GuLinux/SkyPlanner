38dc5948c5272c6fa1a5e26cbca7bbab04c130bb
Marco Gulino <marco@gulinux.net>
2017-01-20 23:48:27 +0000

    Handle validation

--- skyplanner/react-app/skyplanner-loginpage.js
+++ skyplanner/react-app/skyplanner-loginpage.js
@@ -13,11 +13,11 @@ class SkyPlannerLoginPage extends React.Component {
     render() {
        return (
             <form className='container' onSubmit={this.login}>
-                <FormGroup controlId='login-username'>
+                <FormGroup controlId='login-username' validationState={this.state.username_state}>
                     <ControlLabel>Username</ControlLabel>
                     <FormControl name='username' type='text' value={this.state.username} onChange={this.handleChange}/>
                 </FormGroup>
-                <FormGroup controlId='login-password'>
+                <FormGroup controlId='login-password' validationState={this.state.password_state}>
                     <ControlLabel>Password</ControlLabel>
                     <FormControl name='password' type='password' value={this.state.password} onChange={this.handleChange}/>
                 </FormGroup>
@@ -28,20 +28,31 @@ class SkyPlannerLoginPage extends React.Component {
 
     login(e) {
         e.preventDefault();
+
+        if( ['username', 'password'].map( (v) => this.validate(v, this.state[v])  ).some( (v) => !v )  ) {
+            NotificationManager.warning('Please fill the required fields', 'Login', 5000);
+            return;
+        }
         Ajax.send_json('/api/users/login', this.state, 'POST')
             .then(Ajax.decode_json({
-                is_success: this.checkLoginSuccess,
-                success: this.loginSuccess
+                is_success: this.checkLoginSuccess.bind(this),
+                success: this.loginSuccess.bind(this)
         }));
     }
 
     handleChange(e) {
-        this.setState({[e.target.name]: e.target.value})
+        this.validate(e.target.name, e.target.value);
+    }
+
+    validate(controlName, value) {
+        this.setState({[controlName]: value, [controlName + '_state']: value.length > 0 ? undefined : 'error' });
+        return value.length > 0;
     }
 
     checkLoginSuccess(response) {
         if(response.status == 401) {
-            NotificationManager.warning('Login Error', 'Invalid username or password', 5000);
+            this.setState({username_state: 'error', password_state: 'error'});
+            NotificationManager.warning('Invalid username or password', 'Login Error', 5000);
         } 
         return response.status == 200;
     }
