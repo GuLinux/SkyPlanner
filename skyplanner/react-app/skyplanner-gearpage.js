0679524645ede4f3ccd0a1f035ae6c6dd2ceede0
Marco Gulino <marco@gulinux.net>
2017-01-28 22:38:29 +0000

    Gear page - first draft

--- skyplanner/react-app/skyplanner-gearpage.js
+++ skyplanner/react-app/skyplanner-gearpage.js
@@ -1,9 +1,30 @@
 import React from 'react'
 
+class Telescopes extends React.Component {
+    render() {
+        return <p>Telescopes List</p>;
+    }
+}
+
+class TelescopesFrame extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>Telescopes</h2>
+                <Telescopes />
+            </div>
+        );
+    }
+}
+
 class SkyPlannerGearPage extends React.Component {
     render() {
-        return <div> Gear Page rendering </div>;
+        return (
+            <div className='container'>
+                <TelescopesFrame />
+            </div>
+        );
     }
-};
+}
 
 export default SkyPlannerGearPage;
