0c5095227b95b938592deebbcfcfd932e341081c
Marco Gulino <marco@gulinux.net>
2017-01-28 22:43:42 +0000

    Telescopes table with fake data

--- skyplanner/react-app/skyplanner-gearpage.js
+++ skyplanner/react-app/skyplanner-gearpage.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Table } from 'react-bootstrap'
 
-class Telescopes extends React.Component {
+class TelescopeRow extends React.Component {
     render() {
-        return <p>Telescopes List</p>;
+        return (
+            <tr>
+                <td>{this.props.telescope.name}</td>
+                <td>{this.props.telescope.focal_length}</td>
+                <td>{this.props.telescope.diameter}</td>
+            </tr>
+        );
     }
 }
 
@@ -10,10 +17,25 @@ class TelescopesFrame extends React.Component {
     render() {
         return (
             <div>
-                <h2>Telescopes</h2>
-                <Telescopes />
+                <h3>Telescopes</h3>
+                <Table bordered condensed hover>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Focal Length</th>
+                            <th>Diameter</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.rows()}
+                    </tbody>
+                </Table>
             </div>
         );
+    }
+
+    rows() {
+        return this.props.telescopes.map( (t) => <TelescopeRow telescope={t} /> );
     }
 }
 
@@ -21,7 +43,7 @@ class SkyPlannerGearPage extends React.Component {
     render() {
         return (
             <div className='container'>
-                <TelescopesFrame />
+                <TelescopesFrame telescopes={ [{id: 1, name: "Dob", focal_length: 1200, diameter: 300}] }/>
             </div>
         );
     }
